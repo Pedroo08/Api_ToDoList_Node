7164e129c44fc01e9624555e2cb0ceefa016fcc1
PedroGD8 <pedrogamedev8@gmail.com>
2023-08-18 16:42:19 -0300

    Add Controller

---
+++ controllers/TaskController.js
@@ -0,0 +1,87 @@
+const Task = require("../models/Task");
+
+
+exports.CreateTask = async (req, res) => {
+    const { content, status, userId, cratedAt } = req.body;
+  
+    const task = {
+      content,
+      status,
+      userId,
+      cratedAt,
+    };
+  
+    try {
+      await Task.create(task);
+      res.status(200).json({ task });
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  };
+
+
+exports.GetAllTask = async (req, res) => {
+  try {
+    const task = await Task.find();
+
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ erro: error });
+  }
+};
+
+exports.FindByUser = async (req,res) => {
+    const user = req.params.user
+    try{
+        const task = await Task.find({userId:user});
+
+    res.status(200).json(task)
+    
+    }catch (error) {
+        res.status(500).json({ erro: error })
+      }
+
+} 
+
+exports.DeleteTask = async (req,res)=>{
+    const id = req.params.id;
+
+    try{
+        const deletedTask = await Task.findByIdAndDelete(id)
+        res.status(200).json(deletedTask)
+    }catch(error){
+        res.status(500).json({ erro: error })
+      }
+}
+
+exports.UpdateTask = async(req,res) =>{
+    const id = req.params.id;
+
+    const {content,status, userId,cratedAt} = req.body;
+
+    const task = {
+        content,
+        status, 
+        userId,
+        cratedAt 
+
+    }
+
+    try{
+        const Updatedtask = await Task.findByIdAndUpdate(id,task)
+        
+
+        if(Updatedtask.matchedCount === 0){
+            res.status(422).json({message:'Não encontrado'})
+            return
+
+        }
+        res.status(200).json(Updatedtask)
+        
+    }catch(error){
+        res.status(500).json({error:error})
+    }
+
+}
+
+
--- routes/TaskRoute.js
+++ routes/TaskRoute.js
@@ -1,103 +1,23 @@
 const router = require('express').Router();
-const Task = require('../models/Task')
-const User = require('../models/User');
+const TaskController =  require('../controllers/TaskController')
 
-router.post('/', async (req,res) => {
 
-    const {content,status, userId,cratedAt} = req.body;
-    /*let date = new Data()
-    const timeElapsed = Date.now();
-    const today = new Date(timeElapsed);*/
-    const task = {
-        content,
-        status, 
-        userId,
-        cratedAt 
-
-    }
-    //task.cratedAt = today.toDateString();
-    //task.status = 'pedente'
-    try{
-
-        await Task.create(task)
-        res.status(200).json({task})
-        
-    }catch(error){
-        res.status(500).json({error:error})
-    }
-
-    
-})
+//Criar Tarefa
+router.post('/', TaskController.CreateTask)
 
 //Encontrar todos
-router.get('/', async (req,res) => {
+ router.get('/',TaskController.GetAllTask);
 
-    try{
-        const task = await Task.find();
-
-    res.status(200).json(task)
-
-    }catch (error) {
-        res.status(500).json({ erro: error })
-      }
-
-} )
 
 //encontrar por usuario
-router.get('/:user', async (req,res) => {
-    const user = req.params.user
-    try{
-        const task = await Task.find({userId:user});
+router.get('/:user', TaskController.FindByUser)
 
-    res.status(200).json(task)
-    
-    }catch (error) {
-        res.status(500).json({ erro: error })
-      }
-
-} )
+//Deletar Tarefa
+router.delete('/:id', TaskController.DeleteTask)
 
 
-router.delete('/:id', async (req,res)=>{
-    const id = req.params.id;
+//Atualizar Tarefa
+router.patch('/:id', TaskController.UpdateTask)
 
-    try{
-        const deletedTask = await Task.findByIdAndDelete(id)
-        res.status(200).json(deletedTask)
-    }catch(error){
-        res.status(500).json({ erro: error })
-      }
-})
-
-
-router.patch('/:id', async(req,res) =>{
-    const id = req.params.id;
-
-    const {content,status, userId,cratedAt} = req.body;
-
-    const task = {
-        content,
-        status, 
-        userId,
-        cratedAt 
-
-    }
-
-    try{
-        const Updatedtask = await Task.findByIdAndUpdate(id,task)
-        
-
-        if(Updatedtask.matchedCount === 0){
-            res.status(422).json({message:'Não encontrado'})
-            return
-
-        }
-        res.status(200).json(Updatedtask)
-        
-    }catch(error){
-        res.status(500).json({error:error})
-    }
-
-})
 
 module.exports = router;
